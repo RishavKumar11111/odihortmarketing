ce7b13db4e8fcccca1ea31b9e2b7dfbea3b234dd
rishavkumar11111@gmail.com <rishavkumar11111@gmail.com>
2020-04-30 02:33:13 +0530

    eighth commit

--- public/controllers/myHomeCtrl.js
+++ public/controllers/myHomeCtrl.js
@@ -2,9 +2,31 @@ var app = angular.module('myApp', []);
 
 app.controller('myHomeCtrl', function ($scope, $http) {
 
+    $scope.foundFruits = false;
+    $scope.foundVegetables = false;
+    $scope.foundFlowers = false;
+    $scope.foundPlantationCrops = false;
+    $scope.foundSpices = false;
     $scope.getItemDetails = function () {
         $http.get('http://localhost:3000/getItemDetails').then(function success(response) {
             $scope.itemDetails = response.data;
+            angular.forEach($scope.itemDetails, function (i) {
+                if (i.CategoryName == 'Fruits') {
+                    $scope.foundFruits = true;
+                }
+                if (i.CategoryName == 'Vegetables') {
+                    $scope.foundVegetables = true;
+                }
+                if (i.CategoryName == 'Flowers') {
+                    $scope.foundFlowers = true;
+                }
+                if (i.CategoryName == 'Plantation Crops') {
+                    $scope.foundPlantationCrops = true;
+                }
+                if (i.CategoryName == 'Spices') {
+                    $scope.foundSpices = true;
+                }
+            });
         }, function error(response) {
             console.log(response.status);
         }).catch(function err(error) {
